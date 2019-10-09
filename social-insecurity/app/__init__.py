ad1c6f0b021034216cb510d08da84090b6abd256
pio93 <p.koloszyc@stud.uis.no>
2019-10-09 16:05:26 +0200

    fixed remember_me and error handling in friends method

--- social-insecurity/app/__init__.py
+++ social-insecurity/app/__init__.py
@@ -3,14 +3,12 @@ from config import Config
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager, UserMixin
 from flask_sqlalchemy import SQLAlchemy
-from flask_wtf.csrf import CSRFProtect
 import sqlite3
 import os
 
 # create and configure app
 app = Flask(__name__)
 Bootstrap(app)
-csrf = CSRFProtect(app)
 app.config.from_object(Config)
 
 # TODO: Handle login management better, maybe with flask_login?
